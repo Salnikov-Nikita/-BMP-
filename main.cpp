1c4761aae43c50483a899afda8b56eca3d305519
Nikita <nikita@Mac-Studio.local>
2024-09-25 09:32:27 +0300

    switch vector -> unique_ptr

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
+#include <memory>
 using namespace std;
+
 
 class BMP_24 {
    private:
-    vector<vector<int>> bmp;
+    unique_ptr<bool[]> bmp;
+    int width = 0;
+    int height = 0;
 
     template <typename T>
-    T read(istream &f, size_t offset) {
+    T read(istream &f, size_t offset) const {
         T value;
         f.seekg(offset);
         f.read((char *)&value, sizeof(value));
@@ -21,30 +24,34 @@ class BMP_24 {
         ifstream in;
         in.open(file_path, ios::binary | ios::in);
         if (in.is_open()) {
-            int width = read<int32_t>(in, 18);
-            int height = read<int32_t>(in, 22);
+            width = read<int32_t>(in, 18);
+            height = read<int32_t>(in, 22);
             int bpp = read<uint16_t>(in, 28);
             in.seekg(54);
             // cout << "width = " << width << '\n' << "height = " << height << '\n' <<
             // "bpp = " << bpp << endl;
             int row_size = (bpp * width + 31) / 32 * 4;
-            bmp.resize(height);
+            bmp.reset(new bool[height * width]);
+            unique_ptr <char[]> row(new char[row_size]);
+
             for (int i = 0; i < height; i++) {
-                bmp[i].resize(width);
-                vector<char> row(row_size);
-                in.read(row.data(), row_size);
-                for (int j = 0; j < width; j++) bmp[i][j] = row[j * bpp / 8] != 0;
+                in.read(row.get(), row_size);
+                for (int j = 0; j < width; j++) bmp[i * width + j] = row[j * bpp / 8] != 0;
             }
         }
         in.close();
     }
-    void displayBMP() {
-        for (int i = bmp.size() - 1; i >= 0; i--) {
-            for (int j = 0; j < bmp[i].size(); j++) cout << (bmp[i][j] ? "█" : " ");
+    void displayBMP() const {
+        for (int i = height - 1; i >= 0; i--) {
+            for (int j = 0; j < width; j++) cout << (bmp[i * width + j] ? "█" : " ");
             cout << endl;
         }
     }
-    void closeBMP() { bmp.clear(); }
+    void closeBMP() { 
+        bmp.reset(); 
+        width = 0;
+        height = 0;    
+    }
 };
 // ./a.out "/Users/nikita/Git/Консольное приложение, для чтения и отображения
 // BMP файлов/bw.bmp"
